001cb7a276a07b6d99181145615675badbdde51f
Chiranjeevi R <246724548+chiru1016@users.noreply.github.com>
2026-07-16 21:51:11 +0530

    Make products public for customers

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,18 +1,28 @@
 const express = require('express');
-const { 
-  getAllProducts, 
-  getProductById, 
-  addProduct, 
-  updateProduct, 
-  deleteProduct 
+
+const {
+  getAllProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct,
 } = require('../controllers/productController');
+
 const { auth, admin } = require('../middleware/authMiddleware');
+
 const router = express.Router();
 
+/*
+  PUBLIC ROUTES
+  Anyone can see products without login
+*/
 router.get('/', getAllProducts);
 router.get('/:id', getProductById);
 
-// Admin only routes
+/*
+  ADMIN ONLY ROUTES
+  Only admin can add, update, delete products
+*/
 router.post('/', auth, admin, addProduct);
 router.put('/:id', auth, admin, updateProduct);
 router.delete('/:id', auth, admin, deleteProduct);
